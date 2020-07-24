205fc8a22ca08a132f481132d3ead86f039c5434
Waliray <willy2000.wld@gmail.com>
2020-07-23 18:38:17 -0600

    Se agrego constructor a Control

--- src/main/java/Backend/Control.java
+++ src/main/java/Backend/Control.java
@@ -10,5 +10,20 @@ package Backend;
  * @author user-ubunto
  */
 public class Control {
+    private String sequence1;
+    private String sequence2;
+
+    public Control(String sequence1, String sequence2) {
+        this.sequence1 = sequence1.toUpperCase();
+        this.sequence2 = sequence2.toUpperCase();
+    }
+        
+    
+    public String CompareSequences(){
+        String result = "";
+        
+        
+        return result;
+    }
     
 }
